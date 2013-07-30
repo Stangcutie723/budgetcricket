56f6f9d0213b6df640764fd552bc56fd7dc529c5
Diana Pham <dianaphamis@gmail.com>
2013-07-30 10:11:01 -0700

    First commit for Budget Cricket.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bc::Application.config.secret_token = '80b2854d1a69f135ad989f67cf949caa4935b93e437e5c0cc0a448a002dc8b54c2d87c415824d68f85860eca0fabea575e302670e68615742f0731f706ec85c2'
