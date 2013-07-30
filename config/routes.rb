a6affc6224b33da698f3670b6203e760eb67320e
Krystle Frisbie <Stangcutie723@gmail.com>
2013-07-30 13:57:36 -0700

    Welcome Index addition

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Bc::Application.routes.draw do
+  get "welcome/index"
+  root "welcome#index"
+
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ Bc::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
