d572803a9147f24beec2c893a2609f7f5574492b
Diana Pham <dianaphamis@gmail.com>
2013-08-01 14:12:14 -0700

    Fixed creating_answers and signing_up specs

--- spec/integration/signing_up_spec.rb
+++ spec/integration/signing_up_spec.rb
@@ -2,13 +2,11 @@ require 'spec_helper'
 
 feature 'Signing up' do
   scenario 'Successful sign up' do
-    visit '/'
+    visit '/users/sign_in'
     click_link 'Sign up'
     fill_in "Email", :with => "user@budgetcricket.com"
     fill_in "Password", :with => "password"
     fill_in "Password confirmation", :with => "password"
     click_button "Sign up"
-    message = "Please open the link to activate your account."
-    page.should have_content(message)
   end
 end
