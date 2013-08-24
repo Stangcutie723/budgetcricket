52c1a7d30b38045eae0e03da544dbd8777ef2a03
Stangcutie723 <Stangcutie723@gmail.com>
2013-08-23 22:54:24 -0700

    FUCK ME NO MORE SECRET TOKEN ERROR

--- Gemfile
+++ Gemfile
@@ -3,8 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.13'
 gem 'dynamic_form', '1.1.4'
 gem 'devise'
-gem "rack-mini-profiler", "~> 0.1.29"
-
+gem 'figaro'
 
 
 
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,6 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-BudgetCricket::Application.config.secret_token = ENV['RAILS_SECRET']
-#'809817bb7a89d5c0eefeef9447e7b08b2537152b2e36415ffc5aa2b6ea54c1e56d7888be5728e85a0924f4898276f2d1594a0c27fa1e44df3b71aa45cf41c85b'
+BudgetCricket::Application.config.secret_token = '2357e9e9729a88ad8a34d08f3318cb949560aad406874c01e2b79790e1d181e839af003e655e27c598037116c194106208b86bcf53b8807b388eba816ec3ed4c'
 
